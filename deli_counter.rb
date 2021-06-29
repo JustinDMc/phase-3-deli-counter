13ee651f23d731fd9b5296ba9455df0319df296c
Justin <jdanielmcarthur@msn.com>
2021-06-29 14:16:51 -0700

    commit tree a5505dcabde71aed189beb0b35309f57c503dd19

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+# let(:katz_deli) { [] }
+#   let(:other_deli) { ["Logan", "Avi", "Spencer"] }
+#   let(:another_deli) { ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] }
+katz_deli = []
+
+
+
+def line(katz_deli)
+   puts "The line is currently empty."
+end
